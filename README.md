5de2b0404eb6e6cdf148ba5acecd6225b920bd99
Raylander <raylander.guimaraes@gmail.com>
2023-07-08 10:33:35 -0300

    pega imagens da API e cria README

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
+# Cine Guimarães
+
+Um site de cinema, onde exibe os filmes em cartaz e suas informações como, classificação, sinopse, horários das sessões.
+O projeto ainda está em desenvolvimento, estou utilizando ele para estudar e colocar em prática algumas tecnologias.
+
+## Tecnologias utilizadas até o momento
+- Nextjs 13
+- Typescript
+- Tailwindcss
+- TMDB API
+
+
+## Projeto em andamento
+![](/public/assets/Captura%20de%20tela%202023-07-08%20102047.png)
+
+
+
+
+
+
 This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
 
 ## Getting Started
@@ -18,17 +38,5 @@ You can start editing the page by modifying `app/page.tsx`. The page auto-update
 
 This project uses [`next/font`](https://nextjs.org/docs/basic-features/font-optimization) to automatically optimize and load Inter, a custom Google Font.
 
-## Learn More
 
-To learn more about Next.js, take a look at the following resources:
 
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
-
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
-
-## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import { EnvVariables } from "@/types/EnvVariables";
 import { Movie } from "@/types/Movies";
-
+import Link from "next/link";
 import CardFilm from "@/components/CardFilm";
 import Image from "next/image";
 
@@ -26,40 +26,35 @@ export default async function Home() {
   const data = await getData();
 
   const movies: Movie[] = data.results;
-  
 
   return (
-    <main>
+    <main className="max-w-screen-lg mx-auto">
       <div className="flex flex-col items-center justify-center mt-10">
         <h1 className="font-bold text-4xl">FILMES EM CARTAZ</h1>
         <p className="font-semibold text-lg">
           Os melhores filmes você encontra aqui
         </p>
       </div>
-
       <div>
         {/* Fazer aqui um carrossel, nesse carrossel eu preciso da capa do filme, título e classificação, separar essa informação num card, que pode ser criado como component, mais dois botões que mudam os filmes pra esquerda e direita. */}
 
         {/*Ao clicar nesse card, fazer carregar numa div abaixo um player do trailer se possível, a sinopse do filme clicado, demais informacoes do filme, sala e horários disponíveis com botão "comprar" que leva para a tela de comra do ingresso*/}
-        <ul>
+        <ul className="w-full flex flex-wrap gap-4 items-center justify-center pb-4">
           {movies.map((movie) => (
             <li key={movie.id}>
-              <h2>{movie.title}</h2>
-              <div>
-
-                {/* Descobrir como faz para carregar a imagem do postrer do filme */}
-                {/* <Image
-                  src={`${IMAGE_URL}${movie.poster_path}`}
-                  width={300}
-                  height={500}
-                  alt={movie.title}
-                /> */}
-              </div>
+              <Link href="#">
+                <div>
+                  <Image
+                    src={`${IMAGE_URL}${movie.poster_path}`}
+                    width={220}
+                    height={500}
+                    alt={movie.title}
+                  />
+                </div>
+              </Link>
             </li>
           ))}
         </ul>
-
-        <CardFilm />
       </div>
     </main>
   );
