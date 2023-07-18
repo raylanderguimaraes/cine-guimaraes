fc97392b82e2c93eba43fef71461dd26c6990d66
Raylander <raylander.guimaraes@gmail.com>
2023-07-18 10:18:08 -0300

    finaliza swiper dos filmes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ import { useEffect, useState } from "react";
 import { EnvVariables } from "@/types/EnvVariables";
 import { Movie } from "@/types/Movies";
 import CardFilm from "@/components/CardFilm";
-import Image from "next/image";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation, Pagination, A11y } from "swiper/modules";
 
@@ -19,43 +18,48 @@ const env: EnvVariables = {
   IMAGE_URL: process.env.IMAGE_URL || "",
 };
 
-const apiKey = env.apiKey;
-const URL_BASE = env.URL_BASE;
-const IMAGE_URL = env.IMAGE_URL;
+const apiKey = process.env.apiKey || "";
+const URL_BASE = process.env.URL_BASE || "";
 
-export default async function Home() {
+export default function Home() {
+  const [movies, setMovies] = useState<Movie[]>();
+
   async function getData() {
-    const res = await fetch(
-      `https://api.themoviedb.org/3/movie/now_playing?api_key=74c1852101385c9f79e7ba929e418a7e&language=pt-BR&page=1`
+    try {
+      const res = await fetch(
+        `https://api.themoviedb.org/3/movie/now_playing?api_key=74c1852101385c9f79e7ba929e418a7e&language=pt-BR&page=1`
 
-      // Descobrir uma forma de colocar a URL utilizando as variáveis de ambiente
-      // `${URL_BASE}${apiKey}&language=pt-BR&page=1`
-    );
+        // Descobrir uma forma de colocar a URL utilizando as variáveis de ambiente
+        // `${URL_BASE}${apiKey}&language=pt-BR&page=1`
+      );
 
-    const moviesData = await res.json();
-    return moviesData.results;
+      const moviesData = await res.json();
+      setMovies(moviesData.results);
+    } catch (error) {
+      console.log("Erro ao obter os dados dos filmes: ", error);
+    }
   }
 
-  const movies: Movie[] = await getData();
+  useEffect(() => {
+    getData();
+  }, []);
+
+  console.log(movies);
 
   return (
     <main className="max-w-screen-lg mx-auto">
       <div className="flex flex-col items-center justify-center py-8">
         <h1 className="font-bold text-4xl">FILMES EM CARTAZ</h1>
-        <p className="font-semibold text-lg">
+        <p className="font-semibold text-lg text-slate-500">
           Os melhores filmes você encontra aqui
         </p>
       </div>
-
-      {/* Fazer aqui um carrossel, nesse carrossel eu preciso da capa do filme, título e classificação, separar essa informação num card, que pode ser criado como component, mais dois botões que mudam os filmes pra esquerda e direita. */}
-
-      {/* Arrumar uma forma de colocar as informações abaixo de cada imagem do filme, no card, Titulo do filme e classificação */}
 
       <div>
         <Swiper
           className=" h-[430px]"
           modules={[Navigation, Pagination, A11y]}
-          spaceBetween={20}
+          spaceBetween={5}
           slidesPerView={3}
           breakpoints={{
             480: { slidesPerView: 2 },
@@ -64,15 +68,18 @@ export default async function Home() {
           }}
           navigation
           pagination={{ clickable: true }}>
-          {movies.map((movie) => (
-            <SwiperSlide
-              className="!flex justify-center items-center"
-              key={movie.id}>
-              <CardFilm movie={movie} imageUrl={IMAGE_URL} />
-            </SwiperSlide>
-          ))}
+          {movies &&
+            movies.map((movie) => (
+              <SwiperSlide
+                className="!flex justify-center items-center"
+                key={movie.id}>
+                <CardFilm movie={movie} />
+              </SwiperSlide>
+            ))}
         </Swiper>
       </div>
+
+      {/* Fazer aqui uma div que apareça o trailer do filme, sua sinopse e duração do filme, colocando um botão que direciona para uma página de compra do ingresso, que pode ser implementada depois */}
     </main>
   );
 }
